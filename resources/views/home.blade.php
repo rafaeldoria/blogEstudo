46a3f6e962b0192f2e7dac5462402c668df6e070
Rafael Dória <ieieldoria@hotmail.com>
2018-01-25 21:24:29 -0200

    Crud de Usuáruios.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                     <caixa qtd="80" titulo="Artigos" url="{{route('artigos.index')}}" cor="orange" icone="ion ion-pie-graph"></caixa>
                 </div>
                 <div class="col-md-4">
-                    <caixa qtd="1280" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
+                    <caixa qtd="1280" titulo="Usuários" url="{{route('usuarios.index')}}" cor="blue" icone="ion ion-person-stalker"></caixa>
                 </div>
                 <div class="col-md-4">
                     <caixa qtd="3" titulo="Autores" url="#" cor="green" icone="ion ion-person"></caixa>
